77518bb89e65809654b92c66ea3e9e2ed867f3e4
Sahil <sahilgirdhar410@gmail.com>
2024-08-12 12:38:29 -0700

    updated redis config

--- config.py
+++ config.py
@@ -1,8 +1,12 @@
+import os
+
 from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    cache_ttl: int = 60 * 60
+    redis_host: str = os.getenv('REDIS_HOST', 'localhost')
+    redis_port: int = int(os.getenv('REDIS_PORT', 6379))
+    cache_ttl: int = int(os.getenv('CACHE_TTL', 300))
     product_urls: list[str] = [
         "https://shop.lululemon.com/c/womens-leggings/_/N-8r6?format=json",
         "https://shop.lululemon.com/c/accessories/_/N-1z0xcmkZ1z0xl44Z8ok?format=json"
